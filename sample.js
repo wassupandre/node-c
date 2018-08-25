30bbff13f36e17968140c73ce9f3a5c58aa899e2
wassupandre <wassupandre@users.noreply.github.com>
2018-08-25 14:56:01 -0300

    Delete sample.js

--- sample.js
+++
@@ -1,8 +0,0 @@
-var http = require("http")
-
-var porta = "3001"
-var ip = "localhost"
-http.createServer(function(req, res){
-    res.end("<h1>Recebendo request!</h1>")
-}).listen(porta, ip)
-console.log('Servidor rodando na porta ' + porta)
